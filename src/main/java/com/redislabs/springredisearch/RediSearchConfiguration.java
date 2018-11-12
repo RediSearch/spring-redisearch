9aa0042d8ad6d2e44a12bb26ccfe9096127198a6
Julien Ruaux <jruaux@gmail.com>
2018-11-12 15:52:08 -0800

    renamed package

--- src/main/java/com/redislabs/springredisearch/RediSearchConfiguration.java
+++ src/main/java/com/redislabs/springredisearch/RediSearchConfiguration.java
@@ -1,4 +1,4 @@
-package com.redislabs.rediscogs;
+package com.redislabs.springredisearch;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
