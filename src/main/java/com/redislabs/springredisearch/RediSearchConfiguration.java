ebf9343ca0fe38ca309befbb39771938ee16f06f
Julien Ruaux <jruaux@gmail.com>
2019-02-06 23:07:00 -0800

    added bean lifecycle handling

--- src/main/java/com/redislabs/springredisearch/RediSearchConfiguration.java
+++ src/main/java/com/redislabs/springredisearch/RediSearchConfiguration.java
@@ -1,5 +1,7 @@
 package com.redislabs.springredisearch;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -7,8 +9,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.redislabs.lettusearch.RediSearchClient;
+import com.redislabs.lettusearch.StatefulRediSearchConnection;
 
 import io.lettuce.core.RedisURI;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DefaultClientResources;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Getter;
@@ -26,18 +31,57 @@ public class RediSearchConfiguration {
 	private String host;
 	private Integer port;
 	private String password;
+	private Duration timeout;
 
-	@Bean
-	public RediSearchClient getClient() {
-		RedisURI redisURI = RedisURI.create(host == null ? props.getHost() : host,
-				port == null ? props.getPort() : port);
-		if (password != null || props.getPassword() != null) {
-			redisURI.setPassword(password == null ? props.getPassword() : password);
-		}
-		if (props.getTimeout() != null) {
-			redisURI.setTimeout(props.getTimeout());
-		}
-		return RediSearchClient.create(redisURI);
+	@Bean(destroyMethod = "shutdown")
+	ClientResources clientResources() {
+		return DefaultClientResources.create();
 	}
 
+	@Bean(destroyMethod = "shutdown")
+	RediSearchClient client(ClientResources clientResources) {
+		RedisURI redisURI = RedisURI.create(host(), port());
+		String password = password();
+		if (password != null) {
+			redisURI.setPassword(password);
+		}
+		Duration timeout = timeout();
+		if (timeout != null) {
+			redisURI.setTimeout(timeout);
+		}
+		return RediSearchClient.create(clientResources, redisURI);
+	}
+
+	@Bean(destroyMethod = "close")
+	StatefulRediSearchConnection<String, String> connection(RediSearchClient rediSearchClient) {
+		return rediSearchClient.connect();
+	}
+
+	private String host() {
+		if (host == null) {
+			return props.getHost();
+		}
+		return host;
+	}
+
+	private int port() {
+		if (port == null) {
+			return props.getPort();
+		}
+		return port;
+	}
+
+	private String password() {
+		if (password == null) {
+			return props.getPassword();
+		}
+		return password;
+	}
+
+	private Duration timeout() {
+		if (timeout == null) {
+			return props.getTimeout();
+		}
+		return timeout;
+	}
 }
